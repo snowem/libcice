4c868341cdb3367e2486aee4bf68bec253308a15
Jackie Dinh <jackiedinh8@gmail.com>
2017-10-06 14:34:11 +0800

    fix: esp32 misses source files from stun folder

--- component.mk
+++ component.mk
@@ -8,6 +8,7 @@
 #
 COMPONENT_ADD_INCLUDEDIRS := include
 #COMPONENT_PRIV_INCLUDEDIRS :=
+COMPONENT_SRCDIRS := . stun stun/usages
 
 #EXTRA_CFLAGS := -DICACHE_RODATA_ATTR
 #CFLAGS += -Wno-error=implicit-function-declaration -Wno-error=format= -DHAVE_CONFIG_H
